db7fe44584f109765f5ac67429d7049cd9be9ce0
openopentw <b04902053@ntu.edu.tw>
2017-05-29 21:54:24 +0800

    change load data method

--- HW6/hw6.py
+++ HW6/hw6.py
@@ -13,6 +13,7 @@ ID = 1
 train_path  = './data/train.csv'
 test_path   = './data/test.csv'
 output_path = './subm/submission_{}.csv'.format(ID)
+print('Will save output to: {}'.format(output_path))
 
 user_path   = './data/users.csv'
 movie_path  = './data/movies.csv'
@@ -35,7 +36,28 @@ user_data[user_data == 'M'] = 1
 # TODO: combine with train & test data
 # }}}
 # load movie data# {{{
-movie_data = pd.read_csv(movie_data)
+# movie_data = pd.read_csv(movie_path)
+f = open(movie_path, encoding='latin1')
+movie_data = f.readlines()[1:]
+f.close()
+
+movie = []
+genres
+for i in range(len(movie_data)):
+    movie_data[i] = movie_data[i][:-1]
+
+    start = movie_data[i].find(',') + 1
+    delimiter = movie_data[i].find('),') + 1
+
+    title = movie_data[i][start:delimiter]
+    genre = movie_data[i][delimiter+1:]].split('|')
+    movie += [title, genre]
+    genres += [[genre]]
+
+    # TODO: if no bug, remove it
+    if len(movie_data[i]) != 2:
+        print('BUG length: i={}, len={}'.format(i, len(movie_data[i])))
+        break
 # }}}
 '''
 
