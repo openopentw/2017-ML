71694b4216a9227c91237a6fc931b0ea1b8e9caa
openopentw <b04902053@ntu.edu.tw>
2017-05-29 23:40:06 +0800

    vote 9

--- Project/Sberbank Russian Housing Market/vote/vote.py
+++ Project/Sberbank Russian Housing Market/vote/vote.py
@@ -1,21 +1,20 @@
 import numpy as np
 
-ID = 4
+ID = 9
 output_path = '../subm/vote_{}.csv'.format(ID)
 subm_list = [
     '../subm/submission_12.csv',
-    '../subm/submission_12.csv',
-    '../subm/submission_12.csv',
-    '../subm/submission_13.csv',
-    '../subm/submission_17.csv',
-    '../subm/submission_18.csv',
+    '../subm/submission_25.csv',
+    '../subm/submission_26.csv',
 ]
 
 subm = np.zeros((len(subm_list), 7662))
 for i, s in enumerate(subm_list):
     subm[i] = np.genfromtxt(s, delimiter=',')[1:, 1]
 
-y_pred = np.round(np.mean(subm, axis=0)).astype(int)
+y_pred  = np.round(np.mean(subm, axis=0))
+y_pred *= 0.97
+y_pred = y_pred.astype(int)
 
 def save_submission(y_pred):# {{{
     f = open(output_path, 'w')
