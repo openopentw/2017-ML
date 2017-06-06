1b615f22e99b8c934576b0bc501aa2665b303fa8
openopentw <b04902053@ntu.edu.tw>
2017-06-06 13:12:20 +0800

    v 1

---
+++ HW6/vote/vote_csv.py
@@ -0,0 +1,48 @@
+"""
+@author: b04902053
+"""
+# import# {{{
+import pandas as pd
+import numpy as np
+# }}}
+# Parameter #
+ID = 1
+ROUND_DIFF = 0.1
+# argvs# {{{
+print('ID = {}'.format(ID))
+
+output_path = '../subm/vote_{}.csv'.format(ID)
+print('Will save output to: {}'.format(output_path))
+# }}}
+# subm list #
+subm_list = [
+    '../subm/submission_35.csv',
+    '../subm/submission_36.csv',
+]
+# read csvs# {{{
+preds = np.zeros((len(subm_list), 100336))
+for i, subm in enumerate(subm_list):
+    print('loading csv from {}'.format(subm))
+    preds[i] = pd.read_csv(subm)['Rating'].values
+# }}}
+# mean & add something# {{{
+y_pred = np.mean(preds, axis=0)
+# add something
+# TODO ...
+# }}}
+# special rounds# {{{
+y_pred[y_pred < 1] = 1
+y_pred[y_pred > 5] = 5
+
+round_pred = np.round(y_pred)
+diff_pred = np.abs(round_pred - y_pred)
+y_pred[diff_pred < ROUND_DIFF] = np.round( y_pred[diff_pred < ROUND_DIFF] )
+# }}}
+# save to h5 & csv# {{{
+print('Saving submission to {}'.format(output_path))
+f = open(output_path, 'w')
+print('TestDataID,Rating', file=f)
+for i, pred_rate in enumerate(y_pred):
+    print('{},{}'.format(i+1, pred_rate), file=f)
+f.close()
+# }}}
